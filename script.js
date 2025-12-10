bb44edf95a31a100106b40d034fda533d08b7d03
amaniablaglodedata-commits <amaniabla.glodedata@gmail.com>
2025-12-10 11:58:54 +0100

    factscat

--- script.js
+++ script.js
@@ -17,15 +17,52 @@ async function displayCatFacts() {
         const catFacts = await getCatFacts();
         console.log(catFacts);
 
-        const catFactList = document.querySelector("#catFactList");
+        const catFactsList = document.querySelector("#catFactsList");
 
         catFacts.data.forEach(function(item) {
-            const catFactsItem = document.createElement("li");
+            const catFactItem = document.createElement("li");
             catFactItem.textContent = item.fact;
-            catFactList.appendChild(catFactItem);
+            catFactsList.appendChild(catFactItem);
         })
     } catch {
         console.error("Something went wrong");
     }
 }
 displayCatFacts();
+
+//
+//
+
+async function getPokemon() {
+    try {
+        const data = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        console.log(data);
+
+        const response = await data.json();
+        console.log(response);
+        return response;
+    }
+    catch {
+        console.error("Something went wrong");
+    };
+};
+
+async function displayPokemon() {
+    try {
+        const pokemon = await getPokemon();
+        console.log(pokemon);
+        console.log(pokemon.name);
+        console.log(pokemon.sprites);
+        console.log(pokemon.sprites.front_shiny);
+
+        const pokemonCard = document.querySelector("#pokemonCard");
+        pokemonCard.innerHTML = `
+        <h1 class="polemonName">${pokemon.name}</h1>
+        <img class="pokemonImg" src="${pokemon.sprites.front_shiny}" alt="" />
+        `
+    }
+    catch {
+        console.error("Something went wrong");
+    };
+};
+displayPokemon();
